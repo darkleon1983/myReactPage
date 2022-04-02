186fd69021adc1246b69a417e4a5e49b4dce2dd1
Evgeny Korolev <mts9832899777@gmail.com>
2022-04-02 22:29:54 +0700

    Created new repository

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import '../Styles/Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <ul>
+                <li>Home</li>
+                <li>About me</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
