48ae793e6a914c2dbccb7382dbef81345e7308cd
Evgeny Korolev <mts9832899777@gmail.com>
2022-04-03 23:01:07 +0700

    Completed the header part

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,24 +4,33 @@ import '../Styles/Header.css';
 const Header = () => {
     return (
         <div className="header">
-        
-            <ul>
-                <li>
-                    <a href='#'>Home</a>
-                </li>
-                <li>
-                    <a href='#'>About me</a>
-                </li>
-                <li>
-                    <a href='#'>Skills</a>
-                </li>
-                <li>
-                    <a href='#'>Portfolio</a>
-                </li>
-                <li>
-                    <a href='#'>Contacts</a>
-                </li>
-            </ul>
+            <div className='listContainer'>
+                <ul>
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>About me</a>
+                    </li>
+                    <li>
+                        <a href='#'>Skills</a>
+                    </li>
+                    <li>
+                        <a href='#'>Portfolio</a>
+                    </li>
+                    <li>
+                        <a href='#'>Contacts</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='description'>
+                <div className='description-text'>
+                    <div className='description-text_name'>Evgeny <br/> Korolev</div>
+                    <div className='description-text_info'>Frontend developer,<br/> 38 years, Krasnoyarsk</div>
+                    <div className='description-text_lang'>Ru|Eng</div>
+                </div>
+                <div className='description-photo'></div>
+            </div>
         </div>
     );
 }
