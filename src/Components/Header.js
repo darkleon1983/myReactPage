6bb4544482a29b48d145e6f74e47dd75a770f151
Evgeny Korolev <mts9832899777@gmail.com>
2022-04-09 00:50:10 +0700

    completed all markup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,9 +26,17 @@ const Header = () => {
             </div>
             <div className='description'>
                 <div className='description-text'>
-                    <div className='description-text_name'>Evgeny <br/> Korolev</div>
-                    <div className='description-text_info'>Frontend developer,<br/> 38 years, Krasnoyarsk</div>
+                    <div className='description-text_name'>Evgeny <br /> Korolev</div>
+                    <div className='description-text_info'>Frontend developer,<br /> 38 years, Krasnoyarsk</div>
                     <div className='description-text_lang'>Ru|Eng</div>
+                    <div className='mobileButton'>
+                        <svg width="33" height="20" viewBox="0 0 33 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="33" height="3.83721" fill="black" />
+                            <rect y="7.67383" width="33" height="3.83721" fill="black" />
+                            <rect y="15.3496" width="33" height="3.83721" fill="black" />
+                        </svg>
+
+                    </div>
                 </div>
                 <div className='description-photo'></div>
             </div>
